22ed04592b32346fee224d80060cabac886cf85f
Anjali17-10 <anjalidiswar570@gmail.com>
2026-08-08 11:38:25 +0530

    ready to deploy

--- backup/src/Services/authService.js
+++ backup/src/Services/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://interactive-algorithm-visualizer-13.onrender.com/api",
 });
 
 
